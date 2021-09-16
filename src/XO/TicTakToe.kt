30bd55ffd74cc85f98ad1cbf048fc679c90f23c5
extranuts <QroWka8462>
2021-09-16 16:44:42 +0300

    Initial logic

--- src/XO/TicTakToe.kt
+++ src/XO/TicTakToe.kt
@@ -3,3 +3,24 @@ package XO
 fun main() {
     println("Welcome to TicTacToe game")
 }
+
+fun play(game: Game) {
+    //render
+    //get input
+    //act
+
+}
+
+fun render(field: Array<Array<Boolean?>>) {
+
+}
+
+interface Game {
+    val field: Array<Array<Boolean?>>
+    fun act(row: Int, col: Int)
+}
+
+interface Field {
+    val size: Int
+    fun get(row: Int, col: Int): Boolean?
+}
